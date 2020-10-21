a4fba939488390d50f228afcd8048c3872bdcf0b
deepsingh11421 <deepsingh11421@gmail.com>
2020-10-21 15:31:47 +0530

    Updates

---
+++ precache-manifest.c0339a4a8cb8ed9bf173fa72a4a329aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1459148d706cadedb006c11bf3d9186",
+    "url": "/video-app/index.html"
+  },
+  {
+    "revision": "76465951c93770708394",
+    "url": "/video-app/static/css/main.db8806bd.chunk.css"
+  },
+  {
+    "revision": "33f951ea7109cffe4bc6",
+    "url": "/video-app/static/js/2.3aac01ed.chunk.js"
+  },
+  {
+    "revision": "d2ff67a5f290a85b508a595a768d7cfc",
+    "url": "/video-app/static/js/2.3aac01ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76465951c93770708394",
+    "url": "/video-app/static/js/main.ff51a720.chunk.js"
+  },
+  {
+    "revision": "a87ea7a24df6f70bd723",
+    "url": "/video-app/static/js/runtime-main.66cf5e2a.js"
+  }
+]);
