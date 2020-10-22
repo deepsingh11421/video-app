7c794c13cf3deff517fdb4d56ad764523b44f4f1
ajachintya <40269281+ajachintya@users.noreply.github.com>
2020-10-22 13:12:25 +0530

    people

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import VideoModule from './container/videoModule/videoModule';
+import People from './component/People/People';
 
 class App extends Component {
   render() {
     return (
       <div>
         <VideoModule />
+        <People />
       </div>
     );
   }
---
+++ src/component/People/People.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './People.module.css';
+export default function People(name,photo) {
+    return (
+        <div className={classes.People}>
+            <h1>People</h1>
+        </div>
+    )
+}
