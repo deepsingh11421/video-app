eb3b4ae86c82e0b2a76e52150fea84e01f5833b7
deepsingh11421 <deepsingh11421@gmail.com>
2020-11-11 21:10:33 +0530

    hgjh

--- src/component/People/People.js
+++ src/component/People/People.js
@@ -4,6 +4,7 @@ import Person from './Person/Person';
 
 const People = (props) => {
   var list = props.persons;
+  console.log(props.persons);
   var personList = list.map((listKey) => {
     return <Person key={listKey[0]} avatar={listKey[1]} person={listKey[0]}/>;
   });  
